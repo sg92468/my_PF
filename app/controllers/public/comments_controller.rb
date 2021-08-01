3ca71c7be4c7a80dd82006f3635f919e950dbe72
sg92468 <sg92468@gmail.com>
2021-08-01 10:46:11 +0000

    コメント評価追加

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,6 +5,7 @@ class Public::CommentsController < ApplicationController
     portfolio = Portfolio.find(params[:portfolio_id])
     comment = current_user.comments.new(comment_params)
     comment.portfolio_id = portfolio.id
+    comment.score = Language.get_data(comment_params[:comment]) # AIによる自然言語処理の結果
     comment.save
     @comments = portfolio.comments.page(params[:page]).per(5).reverse_order # 非同期のため、コメント一覧データ取出し
   end
--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,9 @@ module MyPf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.i18n.default_locale = :ja
+    config.paths.add 'lib', eager_load: true # 追加
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
