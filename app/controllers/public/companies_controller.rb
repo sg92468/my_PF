6def820424ad33ad3bc10e3573cd26263fd87ebd
sg92468 <sg92468@gmail.com>
2021-08-09 14:10:50 +0000

    チャット開始ボタン作成

--- app/controllers/public/companies_controller.rb
+++ app/controllers/public/companies_controller.rb
@@ -1,5 +1,20 @@
 class Public::CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @companyEntry = Entry.where(company_id: @company.id)
+    @currentUserEntry.each do |cu|
+      @companyEntry.each do |u|
+        if cu.room_id == u.room_id then
+          @isRoom = true
+          @roomId = cu.room_id
+        end
+      end
+    end
+    if @isRoom
+    else
+      @room = Room.new
+      @entry = Entry.new
+    end
   end
 end
