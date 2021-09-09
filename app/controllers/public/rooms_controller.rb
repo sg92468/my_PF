4355ea586eb3fb6c294d0726da63de572ce9deaf
sg92468 <sg92468@gmail.com>
2021-09-09 13:38:23 +0000

    ルームの作成訂正

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -4,20 +4,16 @@ class Public::RoomsController < ApplicationController
 
   def create
     @room = Room.create(params.require(:room).permit(:user_id, :company_id))
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id, company_id: params[:company_id])
+    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id, company_id: @room.company_id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id, :company_id).merge(room_id: @room.id))
     redirect_to "/rooms/#{@room.id}"
   end
 
   def show
     @room = Room.find(params[:id])
-    if Entry.where(user_id: current_user.id, room_id: @room.id, company_id: @room.company_id).present?
       @messages = @room.messages
       @message = Message.new
       @entries = @room.entries
-    else
-      redirect_back(fallback_location: root_path)
-    end
   end
 
 end
